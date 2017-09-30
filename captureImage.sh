a8fdc1996b5d0fc397174b258666c7b37954e546
Sebastian Rosenberg <sebastian@rosenberg.click>
2017-09-30 16:18:01 +0200

    added bash script to fetch picture from android device

---
+++ captureImage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+################################
+#
+#
+#  created by Sebastian Rosenberg <sebastian@rosenberg.click> (c) 2017
+#
+#  script to fetch picture from android camera
+#
+#  requires: adb tool out of android sdk
+#
+################################
+
+
+
+
