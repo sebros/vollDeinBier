fcdfeaae5b78ed0a9ff2f0f9f41cefb2d7cc8f7f
Sebastian Rosenberg <sebastian@rosenberg.click>
2017-10-01 01:21:30 +0200

    working script with fancy output

--- captureImage.sh
+++ captureImage.sh
@@ -10,12 +10,23 @@
 #  requires: adb tool out of android sdk
 #
 #
-#  preparations: lock devices screen orientation to vertical
+#  preperations: lock devices screen orientation to vertical
 #
 ################################
 
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
 
 camera_img_loc="/sdcard/DCIM/Camera"
+last_img=""
+
+if [ "$1" == "" ]; then
+	echo "need first parameter as api key"
+	exit 1
+fi
+
+apiKey=$1
 
 
 if [ "$ANDROID_HOME" == "" ]; then
@@ -27,6 +38,11 @@ export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 if ! type adb > /dev/null 2>&1; then
 	echo "error: adb command not found. Pleas check ANDROID_HOME"
+	exit 1
+fi
+
+if ! type jq > /dev/null 2>&1; then
+	echo -e "error: jq command not found\nyou may try to: apt install jq"
 	exit 1
 fi
 
@@ -94,20 +110,38 @@ function capture_image() {
 	echo "done"
 	
 	
-	echo -e "resizing picture ... "
-	convert ./tmp/$last_img -resize 1024x ./tmp/resized-$last_img > /dev/null > 2>&1
+	echo -n "resizing picture ... "
+	convert ./tmp/$last_img -resize 1024x ./tmp/resized-$last_img > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
-		echo "failed, to resize picture"
+		echo -n "failed, to resize picture"
 		cleanexit
-	fi	
+	fi
+	
+	echo "done"
 	
 	mv ./tmp/$last_img ./archive/original/$last_img
 	mv ./tmp/resized-$last_img ./archive/resized/resized-$last_img
+}
+
+function fetch_vis_prediction() {
+	
+	local img_loc=./archive/resized/resized-$last_img
+
+	#apiKey=$(cat ../watson_apikey.txt);
+	response=$(curl --silent -X POST --form "images_file=@$img_loc" --form "parameters=@classes.json" "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=$apiKey&version=2016-05-20" | tee out.log | jq '.images[].classifiers[].classes[].class' | cut -d"\"" -f2)
+	
+	echo ""
+
+	if [ "$response" == "biervoll" ]; then
+		printf "${GREEN}There is enought beer in the glass.${NC}\n\n"
+	else
+		printf "${RED}Glass seems to be emtpy.${NC}\n\n"
+	fi
 }
 
 
 
 ### start here
 capture_image
-
+fetch_vis_prediction
 
