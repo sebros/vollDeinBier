b6cb6ae31fe1853acc352879f81a637a030d1ad5
skrengel <mail@skrengel.de>
2017-10-01 02:49:10 +0100

    Native App ueber Ionic Framework

---
+++ ionicApp/src/pages/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera } from 'ionic-native';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+	public base64Image: string;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+	takePicture(){
+		Camera.getPicture({
+			destinationType: Camera.DestinationType.DATA_URL,
+			targetWidth: 1000,
+			targetHeight: 1000
+		}).then((imageData) => {
+			// imageData is a base64 encoded string
+			this.base64Image = "data:image/jpeg;base64," + imageData;
+		}, (err) => {
+			console.log(err);
+		});
+	}
+
+}
